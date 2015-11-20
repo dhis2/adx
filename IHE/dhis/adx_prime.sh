7ad1db4749cd4ecf3310ee3b8ee8a88deca443af
bobjolliffe <bobjolliffe@gmail.com>
2015-11-20 14:43:34 +0000

    Minor improvement to script.  Changed code to CODE on import options.

--- IHE/dhis/adx_prime.sh
+++ IHE/dhis/adx_prime.sh
@@ -3,30 +3,30 @@
 # It uses dhis2-tools to start and stop the test instance
 # It should be easily modified to suit your test environment
 
-DBNAME=adxtest
+DBNAME=twenty
 DBOWNER=twenty
 # The name of the dhis instance
 DHISNAME=twenty
 # The url of the test instance
-DHISURL="http://localhost:8081/twenty"
+DHISURL="http://localhost:8080/twenty"
 
 echo "Shutdown instance if running"
-dhis2-shutdown twenty
+dhis2-shutdown $DHISNAME
 sleep 3
 # Start with a blank database
 echo "Setting up database"
-dropdb adxtest
-createdb -O twenty adxtest
+dropdb $DBNAME
+createdb -O $DBOWNER $DBNAME
 # Load some sample metadata
-cat adx_sample.sql |psql adxtest
-psql -c "REASSIGN OWNED BY $USER TO twenty" adxtest
+cat adx_sample.sql |psql $DBNAME
+psql -c "REASSIGN OWNED BY $USER TO $DBOWNER" $DBNAME
 
 echo "starting dhis2"
-dhis2-startup twenty
+dhis2-startup $DHISNAME
 
 echo "wait a while for tomcat to get its listening port open"
 sleep 10
 
 echo "Posting adx data - will take a while as dhis2 starts up"
-curl -u admin:district -X POST -H "Content-Type: application/xml" -d @data.xml "$DHISURL/ohie/dataValueSets?dataElementIdScheme=code&orgUnitIdScheme=code" |xmllint --format -
+curl -u admin:district -X POST -H "Content-Type: application/xml" -d @data.xml "$DHISURL/ohie/dataValueSets?dataElementIdScheme=CODE&orgUnitIdScheme=CODE" |xmllint --format -
 
